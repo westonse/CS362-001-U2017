6c825da8c7946f0c67ef08979184ceee64224ee9
Seth Weston <westonse@Seths-MacBook-Pro.local>
2017-08-03 16:52:59 -0700

    adding assignment 3

---
+++ projects/westonse/assignment-3/Calendar/src/test/java/calendar/ApptTest.java
@@ -0,0 +1,47 @@
+package calendar;
+/**
+ *  This class provides a basic set of test cases for the
+ *  Appt class.
+ */
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class ApptTest {
+    /**
+     * Test that the gets methods work as expected.
+     */
+	 @Test
+	  public void test01()  throws Throwable  {
+		 int startHour=13;
+		 int startMinute=30;
+		 int startDay=10;
+		 int startMonth=4;
+		 int startYear=2017;
+		 String title="Birthday Party";
+		 String description="This is my birthday party.";
+		 //Construct a new Appointment object with the initial data	 
+		 Appt appt = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+	// assertions
+		 assertTrue(appt.getValid());
+		 assertEquals(13, appt.getStartHour());
+		 assertEquals(30, appt.getStartMinute());
+		 assertEquals(10, appt.getStartDay());
+		 assertEquals(04, appt.getStartMonth());
+		 assertEquals(2017, appt.getStartYear());
+		 assertEquals("Birthday Party", appt.getTitle());
+		 assertEquals("This is my birthday party.", appt.getDescription());         		
+	 }
+
+	 @Test
+	  public void test02()  throws Throwable  {
+		 
+	 }
+//add more unit tests as you needed
+	
+}
